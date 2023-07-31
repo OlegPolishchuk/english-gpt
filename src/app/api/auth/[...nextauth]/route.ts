62accc21d4be8f79118835e1a4f2c32e37be3246
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-07-31 13:46:31 +0300

    fix: [...nextauth]/route.ts handler for vercel deployment

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,56 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
+import { async } from 'regenerator-runtime';
 
 import { authConfig } from '@/configs';
 import { insertUserToDb, findUser } from '@/server/services/db';
 import { UserFromNextAuth } from '@/server/types';
 import { createNewUser } from '@/server/utils';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return await NextAuth(req, res, {
-    ...authConfig,
-    callbacks: {
-      session({ session }) {
-        (async () => {
-          const { user } = session;
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   return await NextAuth(req, res, {
+//     ...authConfig,
+//     callbacks: {
+//       session({ session }) {
+//         (async () => {
+//           const { user } = session;
+//
+//           if (user && user.email) {
+//             const userFromDb = await findUser(user.email);
+//
+//             if (!userFromDb) {
+//               const newUser = createNewUser(user as UserFromNextAuth);
+//               await insertUserToDb(newUser);
+//             }
+//           }
+//         })();
+//
+//         return session;
+//       },
+//     },
+//   });
+// };
 
-          if (user && user.email) {
-            const userFromDb = await findUser(user.email);
+const handler = NextAuth({
+  ...authConfig,
+  callbacks: {
+    session({ session }) {
+      (async () => {
+        const { user } = session;
 
-            if (!userFromDb) {
-              const newUser = createNewUser(user as UserFromNextAuth);
-              await insertUserToDb(newUser);
-            }
+        if (user && user.email) {
+          const userFromDb = await findUser(user.email);
+
+          if (!userFromDb) {
+            const newUser = createNewUser(user as UserFromNextAuth);
+            await insertUserToDb(newUser);
           }
-        })();
+        }
+      })();
 
-        return session;
-      },
+      return session;
     },
-  });
-};
+  },
+});
 
 export { handler as GET, handler as POST };
