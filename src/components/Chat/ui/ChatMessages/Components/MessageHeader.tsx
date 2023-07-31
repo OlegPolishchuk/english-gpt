0b664bb9ebb9ae8e3ff5c8626a1e9a2d78965284
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-07-31 15:04:55 +0300

    feat: add User name to chat message if user is auth

--- src/components/Chat/ui/ChatMessages/Components/MessageHeader.tsx
+++ src/components/Chat/ui/ChatMessages/Components/MessageHeader.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import clsx from 'clsx';
+import { useSession } from 'next-auth/react';
 
 import cls from '../ChatMessages.module.css';
 
@@ -13,10 +14,14 @@ interface Props {
   message: string;
 }
 export const MessageHeader = ({ isUser, message }: Props) => {
+  const { data } = useSession();
+
+  const userName = data?.user?.name ?? 'User';
+
   return (
     <div className={cls.header}>
       <h3 className={clsx(cls.userName, isUser ? cls.user : '')}>
-        {isUser ? 'User' : 'ChatGPT'}
+        {isUser ? userName : 'ChatGPT'}
       </h3>
 
       <div className={clsx(cls.icon_sound, isUser && cls.isUser)}>
