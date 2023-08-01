59e7ecc9f7370c73a22d88154d4442a3d3514a37
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-08-01 10:13:04 +0300

    fix: break-word in message body

--- src/components/Chat/ui/ChatMessages/Components/MessageBody.tsx
+++ src/components/Chat/ui/ChatMessages/Components/MessageBody.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import cls from '../ChatMessages.module.css';
+
 interface Props {
   message: string;
 }
 export const MessageBody = ({ message }: Props) => {
-  return <p>{message}</p>;
+  return <p className={cls.text_container}>{message}</p>;
 };
--- src/modules/Chat/ui/Chat.tsx
+++ src/modules/Chat/ui/Chat.tsx
@@ -1,20 +1,12 @@
-'use client';
-
 import React from 'react';
-
-import { useSetMobileHeight } from '../hooks';
 
 import cls from './Chat.module.css';
 
 import { ChatControls, MessagesField } from '@/components/Chat';
 
-const HEADER_HEIGHT = 65;
-
 export const Chat = () => {
-  const height = useSetMobileHeight();
-
   return (
-    <section className={cls.chat} style={{ height: `${height - HEADER_HEIGHT}px` }}>
+    <section className={cls.chat}>
       <MessagesField />
 
       <ChatControls />
