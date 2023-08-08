3d1e9160e9f75a011a1bfbea76e3f96d843fb9e8
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-08-08 13:49:30 +0300

    refactor: remove unused types

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import { UserFromNextAuth } from '@/server/types';
 import { Nullable } from '@/types/common';
 
 export interface User {
@@ -8,12 +7,3 @@ export interface User {
   created: Date;
   image?: ImgSrs;
 }
-
-export const createNewUser = (user: UserFromNextAuth): User => {
-  return {
-    email: user.email,
-    name: user.name,
-    image: user.image,
-    created: new Date(),
-  };
-};
--- src/models/UserActivity.ts
+++ src/models/UserActivity.ts
@@ -7,14 +7,3 @@ export interface UserActivity {
   last_visit: DateString;
   dates_of_visits: DateString[];
 }
-
-export const createNewActivities = (userEmail: string): UserActivity => {
-  return {
-    user_email: userEmail,
-    last_visit: new Date().toISOString(),
-    total_count_of_visits: 1,
-    week_count_of_visits: 1,
-    consecutive_visits: 1,
-    dates_of_visits: [new Date().toISOString()],
-  };
-};
