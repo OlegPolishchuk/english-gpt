89fe53310760c6ecee9430a2da476133b9ee0068
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-08-08 14:11:34 +0300

    fix: add dynamic=force-dynamic for vercel

--- src/services/fetchService.ts
+++ src/services/fetchService.ts
@@ -1,6 +1,9 @@
 import {cookies} from "next/headers";
 
+export const dynamic = "force-dynamic" //clodflare solution
+
 export const fetchAPI = async (url: string, init?: RequestInit ) => {
+
   const options = {
     method: 'GET',
     credentials: 'include',
