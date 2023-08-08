8b44e061ac032819aa5e844a3bb26fe66992973d
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-08-08 15:46:49 +0300

    feat: add logs for vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,9 @@ import {Routes} from "@/shared/constants";
 
 
 export async function middleware(req: NextRequest) {
+  console.log('middleware')
   const token = req.cookies.get('accessToken');
-
+  console.log('token =>', token)
   if (!token) {
     return NextResponse.redirect(new URL(Routes.signin, req.url));
   }
